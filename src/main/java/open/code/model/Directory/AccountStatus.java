165e23cdfafe4ece003d567c68e08b07ec34c662
wonderfullss <dkrechin@list.ru>
2023-06-07 21:12:46 +0400

    update

--- src/main/java/open/code/model/Directory/AccountStatus.java
+++ src/main/java/open/code/model/Directory/AccountStatus.java
@@ -1,12 +1,20 @@
 package open.code.model.Directory;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountStatus {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/open/code/model/Directory/InfoTypeCode.java
+++ src/main/java/open/code/model/Directory/InfoTypeCode.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor

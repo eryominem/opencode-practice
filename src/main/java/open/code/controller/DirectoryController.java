7de8744f67e978f272b88f5fb6dc9015320155c7
wonderfullss <dkrechin@list.ru>
2023-06-07 20:55:09 +0400

    update

--- src/main/java/open/code/controller/DirectoryController.java
+++ src/main/java/open/code/controller/DirectoryController.java
@@ -1,9 +1,17 @@
 package open.code.controller;
 
+import open.code.service.DirectoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/directory/**")
 public class DirectoryController {
+    private final DirectoryService directoryService;
+
+    @Autowired
+    public DirectoryController(DirectoryService directoryService) {
+        this.directoryService = directoryService;
+    }
 }

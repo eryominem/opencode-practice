c8f28b679d5a403d26761f6893c3cbb6cfde170d
eryominem <old.wiss@yandex.ru>
2023-06-08 17:32:34 +0400

    directory controller

---
+++ src/main/java/open/code/controller/DirectoryController.java
@@ -0,0 +1,32 @@
+package open.code.controller;
+
+import open.code.model.Directory;
+import open.code.dto.DirectoryDto;
+import open.code.service.directory.DirectoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/directories")
+public class DirectoryController {
+    private final DirectoryService directoryService;
+
+    @Autowired
+    public DirectoryController(DirectoryService directoryService) {
+        this.directoryService = directoryService;
+    }
+
+    @PostMapping("/{type}")
+    public ResponseEntity<Directory> add(@RequestBody DirectoryDto directoryDto,
+                                         @PathVariable("type") String directoryType) {
+        return directoryService.add(directoryDto, directoryType);
+    }
+
+    @GetMapping("/{type}")
+    public List<Directory> getAll(@PathVariable("type") String directoryType) {
+        return directoryService.getAll(directoryType);
+    }
+}

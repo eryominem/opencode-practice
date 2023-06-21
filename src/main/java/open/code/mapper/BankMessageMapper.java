7f27f2ca27105198e86e08f66e067b5bad3bbb3b
wonderfullss <dkrechin@list.ru>
2023-06-21 17:02:27 +0400

    mapper

--- src/main/java/open/code/mapper/BankMessageMapper.java
+++ src/main/java/open/code/mapper/BankMessageMapper.java
@@ -4,11 +4,17 @@ import open.code.dto.BankMessageDto;
 import open.code.model.BankMessage;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring",
         uses = {BicDirectoryMapper.class, ParticipantMapper.class, AccountMapper.class, SWBICSMapper.class},
         injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface BankMessageMapper {
+
+    @Mapping(target = "eDNo", source = "EDNo")
+    @Mapping(target = "eDDate", source = "EDDate")
+    @Mapping(target = "eDAuthor", source = "EDAuthor")
+    @Mapping(target = "eDReceiver", source = "EDReceiver")
     BankMessage toModel(BankMessageDto bankMessageDto);
 
     BankMessageDto toDTO(BankMessage bankMessage);
